deb96c7b320202d0933c81268b5f127058def872
Oleg Vashkevich <ugluk@tut.by>
2025-02-06 09:12:06 +0300

    php переписан под XMLReader

--- index.php
+++ index.php
@@ -2,29 +2,56 @@
 
 if (isset($argv[1])) {
     $file = "epg2.xml.gz";
-    //check file date
+    // Check file date
     if (!file_exists($file) || filemtime("epg2.xml.gz") < time() - 86400) {
-        //load file from web
+        // Load file from web
         $url = "http://epg.one/epg2.xml.gz";
         file_put_contents($file, file_get_contents($url));
     }
-    
-    $xml = simplexml_load_file("compress.zlib://{$file}");
-    $channels = [];
-    foreach ($xml->channel as $c) {
-        $channels[ $c['id']->__toString() ] = $c->{'display-name'}->__toString();
-    }
     $fraze = mb_strtolower($argv[1]);
+
+    $reader = new XMLReader();
+    $reader->open("compress.zlib://{$file}");
+
+    $channels = [];
     $founds = [];
-    foreach ($xml->programme as $item) {
-        $title = mb_strtolower($item->title);
-        if (strpos($title, $fraze) !== false) {
-            $start = substr((string)$item["start"], 0, -8);
-            $end   = substr((string)$item["stop"], 0, -8);
-            $founds[$channels[(int)$item['channel']]][] = date("G:i", strtotime($start)).' - '.date("G:i d.m.Y", strtotime($end)).' '.$item->title->__toString();
+
+    while ($reader->read()) {
+        if ($reader->nodeType == XMLReader::ELEMENT) {
+            switch ($reader->name) {
+                case 'channel':
+                    $channel_id = (int)$reader->getAttribute('id');
+                    while ($reader->read()) {
+                        if ($reader->nodeType == XMLReader::ELEMENT && $reader->name == 'display-name') {
+                            $channel_name = (string)$reader->readInnerXML();
+                            $channels[$channel_id] = $channel_name;
+                            break;
+                        }
+                    }
+                    break;
+
+                case 'programme':
+                    $start = substr((string)$reader->getAttribute('start'), 0, -8);
+                    $end = substr((string)$reader->getAttribute('stop'), 0, -8);
+                    $channel = $channels[(int)$reader->getAttribute('channel')];
+                    $title = '';
+                    while ($reader->read()) {
+                        if ($reader->nodeType == XMLReader::ELEMENT && $reader->name == 'title') {
+                            $title = mb_strtolower((string)$reader->readInnerXML());
+                            break;
+                        }
+                    }
+                    if (strpos($title, $fraze) !== false) {
+                        $founds[$channel][] = date("G:i", strtotime($start)) . ' - ' . date("G:i d.m.Y", strtotime($end)) . ' ' . (string)$reader->value;
+                    }
+                    break;
+            }
+        } elseif ($reader->nodeType == XMLReader::END_ELEMENT && $reader->name == 'channel') {
+            continue; // Skip to the next channel
         }
     }
-    
+
+    $reader->close();
     print_r($founds);
 } else {
     echo "Usage: php index.php <search_word>\n";
